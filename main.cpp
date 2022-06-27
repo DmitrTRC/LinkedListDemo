eea161419b8a30891686128cc233a0da6d394718
Dev Spain <9984398@gmail.com>
2022-06-27 21:07:36 +0300

    Lesson2 ( LinkedList ) : completed insertAtBegin() : Done

--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
 #include <iostream>
 
 
-class Point {
-public:
-    float x;
-    float y;
-
-    Point (float x, float y) {
-        this->x = x;
-        this->y = y;
-    }
-
-    friend std::ostream &operator<< (std::ostream &os, const Point &p) {
-        os << "(" << p.x << ", " << p.y << " Difference: " << p.x - p.y << ")";
-        return os;
-    }
-};
-
 class Node {
 public:
-    Point* data;
+    int data;
     Node *next;
 };
 
@@ -31,31 +15,55 @@ void printList (Node *n) {
 }
 
 // data =  { 100, 200, 300 , 400 }
-int main () {
-    Node *Head = new Node;
-    Node *secondNode = new Node;
-    Node *thirdNode = new Node;
-    Node *fourthNode = new Node;
 
-    Head->data = new Point (1, 1);
+typedef Node *nodePtr;
+
+//TODO: Implement the function
+void push_back( nodePtr *Head, int value){
+
+}
+
+void insertAtBegin (nodePtr *head, int value) {
+    // Prepare a new node
+    nodePtr tmpNode = new Node;
+
+    // Set node value
+    tmpNode ->data = value;
+
+    // Set tmpNode before current Head
+    tmpNode ->next = *head;
+
+    *head  = tmpNode;
+
+}
+
+int main () {
+    nodePtr Head = new Node;
+    nodePtr secondNode = new Node;
+    nodePtr thirdNode = new Node;
+    nodePtr fourthNode = new Node;
+
+    Head->data = 1;
     Head->next = secondNode;
 
-    secondNode->data = new Point (2, 2);
+    secondNode->data = 2;
     secondNode->next = thirdNode;
 
-    thirdNode->data = new Point (3, 3);
+    thirdNode->data = 3;
     thirdNode->next = fourthNode;
 
-    fourthNode->data = new Point (4, 4);
+    fourthNode->data = 4;
     fourthNode->next = NULL;
 
     printList (Head);
 
-    Point point1 = {100, 100}; //Stack
-    Point *point2 = new Point (200, 200);//Heap;
+    std::cout << "Adding two elements -1 , 100 " << std::endl;
 
-    std::cout << point1 << std::endl;
-    std::cout << *point2 << std::endl;
+    insertAtBegin (&Head, -1);
+    insertAtBegin (&Head, 100);
+
+    printList (Head);
+
 
 
     return 0;
